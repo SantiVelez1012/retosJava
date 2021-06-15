fbcdde9f97342a4b6b1ca11dc1d15af1c1ea7be2
yontaru <70823176+yontaru@users.noreply.github.com>
2021-06-15 07:49:12 -0500

    Initial commit

---
+++ src/retosxd/Retosxd.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package retosxd;
+
+import java.util.Scanner;
+
+
+public class Retosxd {
+
+    
+    
+    public static void main(String[] args) {
+    
+        Scanner teclado = new Scanner(System.in);
+        
+        /*float juan, camila, ricardo;
+        
+        System.out.println("Ingrese el dinero de Juan: ");
+        juan = teclado.nextFloat();
+        
+        camila = juan / 2;
+        
+        ricardo = (camila + juan)/2;
+        
+        System.out.println("La cantidad de dinero que tiene juan es " + juan +
+                ". La cantidad de dinero que tiene Camila es " + camila +
+                ". Y la cantidad que posee Ricardo es " + ricardo );*/
+        
+        
+        
+        
+        
+        
+    }
+    
+}
